f6a9da012bbc626d49421354e266c8721bf43c46
Javad <sir.hxt@gmail.com>
2023-09-15 09:42:35 +0330

    Initial Commit

---
+++ iOS 17 Interactable Widgets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS 17 Interactable Widgets
+//
+//  Created by Javad on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS 17 Interactable Widgets/iOS_17_Interactable_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_17_Interactable_WidgetsApp.swift
+//  iOS 17 Interactable Widgets
+//
+//  Created by Javad on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_17_Interactable_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
